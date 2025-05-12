2251929584c29cfebf2f149ccc26bd085ff91ab4
Lior Ben-David <lior@hotglue.io>
2025-05-12 17:37:18 +0000

    HGI-7841 - Add backoff/retry logic  using error message

--- tap_precoro/client.py
+++ tap_precoro/client.py
@@ -1,13 +1,15 @@
 """REST client handling, including PrecoroStream base class."""
 
 import requests
+import json
 from pathlib import Path
 from typing import Any, Dict, Optional, Union, List, Iterable
-
+import time
 from memoization import cached
-
+import pendulum
 from singer_sdk.streams import RESTStream
 from singer_sdk.authenticators import APIKeyAuthenticator
+from singer_sdk.exceptions import RetriableAPIError, FatalAPIError
 from pendulum import parse
 
 
@@ -83,3 +85,25 @@ class PrecoroStream(RESTStream):
         if self.replication_key and start_date:
             params["modifiedSince"] = start_date.strftime('%Y-%m-%dT%H:%M:%S') 
         return params
+
+    def validate_response(self, response: requests.Response) -> None:
+        if response.status_code == 429 and 'RateLimit-Type' in response.text:
+            seconds_to_wait = 1
+            try:
+                retry_info_json = response.json()
+                rate_limit_type = retry_info_json.get("RateLimit-Type")
+                if rate_limit_type == "Daily limiter":
+                    self.logger.error("Daily rate limit hit. Exiting.")
+                    raise FatalAPIError("Daily rate limit hit. Exiting.")
+
+                datetime_str = retry_info_json.get('RateLimit-Retry-After')
+                retry_after = pendulum.from_format(datetime_str, 'YYYY-MM-DD HH:mm:ss z')
+                seconds_to_wait = (retry_after - pendulum.now()).total_seconds()
+                seconds_to_wait = max(seconds_to_wait, 1)
+            except Exception as e:
+                self.logger.warning(f"Could not parse retry info: {response.text}")
+                self.logger.warning(f"Error parsing retry info: {e}")
+            self.logger.info(f"Waiting {seconds_to_wait} seconds before retrying")
+            time.sleep(seconds_to_wait)
+            raise RetriableAPIError("Rate limit exceeded", response=response)
+        super().validate_response(response)
